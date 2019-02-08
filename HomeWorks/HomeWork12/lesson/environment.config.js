c00be7d177488c54cf20f4c6aadf204e40d6fca2
MuravlevaSvetlana <46105634+MuravlevaSvetlana@users.noreply.github.com>
2019-02-08 17:48:41 +0200

    Create environment.config.js

---
+++ HomeWorks/HomeWork12/lesson/environment.config.js
@@ -0,0 +1,21 @@
+class ENV {
+    static get apiUrl() {
+        return  'https://newsapi.org/v2';
+    }
+
+    static get apiKey() {
+        return '67da102c43404e0891b286a39b4d83e6';
+    }
+
+    static get country() {
+        return 'ua';
+    }
+     
+    static get category() {
+        return 'technology';
+    } 
+
+    static get keyWord() {
+        return 'Ukraine';
+    }
+}
